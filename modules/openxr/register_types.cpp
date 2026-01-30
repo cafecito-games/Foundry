134db70a6e1dee82ec99269b28dca124c102aa41
Thaddeus Crews <repiteo@outlook.com>
2026-01-30 09:32:10 -0600

    Merge pull request #115602 from dsnopek/openxr-user-presence-hide-singleton
    Don't expose the `OpenXRUserPresenceExtension` singleton
    
    (cherry picked from commit 82580cf965caf5ce75fc86d4d26e68071bfe4685)

--- modules/openxr/register_types.cpp
+++ modules/openxr/register_types.cpp
@@ -135,7 +135,6 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 		FOUNDRY_REGISTER_CLASS(OpenXRRenderModelExtension);
 #endif
 		FOUNDRY_REGISTER_CLASS(OpenXRAndroidThreadSettingsExtension);
-		FOUNDRY_REGISTER_CLASS(OpenXRUserPresenceExtension);
 
 		// Note, we're not registering all wrapper classes here, there is no point in exposing them
 		// if there isn't specific logic to expose.
@@ -211,7 +210,6 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 			// Register user presence extension as a singleton
 			OpenXRUserPresenceExtension *user_presence_extension = memnew(OpenXRUserPresenceExtension);
 			OpenXRAPI::register_extension_wrapper(user_presence_extension);
-			Engine::get_singleton()->add_singleton(Engine::Singleton("OpenXRUserPresenceExtension", user_presence_extension));
 
 			// register gated extensions
 			if (int(GLOBAL_GET("xr/openxr/extensions/debug_utils")) > 0) {
