483dd23177074a41150fdb83540cd9adba40d9fb
Thaddeus Crews <repiteo@outlook.com>
2026-02-04 11:04:22 -0600

    Merge pull request #115868 from bruvzg/aslr_load_addr_fix
    [macOS] Set `main` visibility to default.
    
    (cherry picked from commit a6623908171440f5d485430464b7ed04d85690cb)

--- platform/macos/foundry_main_macos.mm
+++ platform/macos/foundry_main_macos.mm
@@ -39,7 +39,7 @@
 #include <sys/resource.h>
 #endif
 
-int main(int argc, char **argv) {
+__attribute__((visibility("default"))) int main(int argc, char **argv) {
 	foundry_init_profiler();
 
 #if defined(VULKAN_ENABLED)
