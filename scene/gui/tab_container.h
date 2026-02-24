eb17fd340a75ecb0c4b1260111cfe0980fd7ee23
Thaddeus Crews <repiteo@outlook.com>
2026-02-24 09:29:31 -0600

    Merge pull request #116681 from KoBeWi/get_one_tab
    Improve `get_tab_control()` in TabContainer
    
    (cherry picked from commit 9a39e0c6e729fff10627ac55c2bf02614f1018e0)

--- scene/gui/tab_container.h
+++ scene/gui/tab_container.h
@@ -119,6 +119,7 @@ private:
 
 	Rect2 _get_tab_rect() const;
 	int _get_tab_height() const;
+	Control *_as_tab_control(Node *p_child) const;
 	Vector<Control *> _get_tab_controls() const;
 	void _on_theme_changed();
 	void _repaint();
