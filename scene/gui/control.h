9862c2260842e303934f611cfe4a81f1549cb5cd
Rémi Verschelde <rverschelde@gmail.com>
2026-03-16 23:41:08 +0100

    Merge pull request #111819 from MewPurPur/democratize-cursor-shape
    Add virtual `_get_cursor_shape()` method in Control
    
    (cherry picked from commit 1a69be88e2a64cefc5c2bfbb9e60f5a080a9ced7)

--- scene/gui/control.h
+++ scene/gui/control.h
@@ -405,6 +405,8 @@ protected:
 	FOUNDRY_VIRTUAL2(_drop_data, Vector2, Variant)
 	FOUNDRY_VIRTUAL1RC(Object *, _make_custom_tooltip, String)
 
+	FOUNDRY_VIRTUAL1RC(int, _get_cursor_shape, Vector2)
+
 	FOUNDRY_VIRTUAL0RC(String, _accessibility_get_contextual_info);
 	FOUNDRY_VIRTUAL1RC(String, _get_accessibility_container_name, RequiredParam<const Node>)
 
@@ -640,7 +642,7 @@ public:
 
 	void set_default_cursor_shape(CursorShape p_shape);
 	CursorShape get_default_cursor_shape() const;
-	virtual CursorShape get_cursor_shape(const Point2 &p_pos = Point2i()) const;
+	virtual CursorShape get_cursor_shape(const Point2 &p_pos = Point2()) const;
 
 	void set_clip_contents(bool p_clip);
 	bool is_clipping_contents();
