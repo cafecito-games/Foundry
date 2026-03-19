d6ebc48c30ccc87940bea9bf0926906eda4b18a5
Thaddeus Crews <repiteo@outlook.com>
2026-03-19 10:20:01 -0500

    Merge pull request #117355 from m4gr3d/bump_vulkan_requirements
    Android: Bump the minimum api version and hardware version for Vulkan support
    (cherry picked from commit e3173f4ec4327e57a4a9b4d5eb3745f7252c1355)

--- platform/android/java/lib/src/main/java/games/cafecito/foundry/Foundry.kt
+++ platform/android/java/lib/src/main/java/games/cafecito/foundry/Foundry.kt
@@ -999,8 +999,8 @@ class Foundry private constructor(val context: Context) {
 			Log.w(TAG, "The vulkan hardware level does not meet the minimum requirement: 1")
 		}
 
-		// Check for api version 1.0
-		return packageManager.hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_VERSION, 0x400003)
+		// Check for api version 1.1
+		return packageManager.hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_VERSION, 0x401000)
 	}
 
 	private fun setKeepScreenOn(enabled: Boolean) {
