d2ab49d66436b402818f17c4451c4746317288ed
Christian Sueiras <1381470+csueiras@users.noreply.github.com>
2026-06-23 20:36:37 -0400

    Document GDScript namespace imports

---
+++ modules/gdscript/tests/scripts/analyzer/features/issue_69_namespace_doc_example.norun.gd
@@ -0,0 +1,5 @@
+import issue_69_docs.game.characters
+
+var player: Issue69DocPlayer
+var controller: controllers.Issue69DocPlayerController
+var deep_controller: controllers.deep.Issue69DocDeepController
