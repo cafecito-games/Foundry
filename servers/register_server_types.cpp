godotengine/godot

Refactor blas_create to accept multiple geometries.

servers/register_server_types.cpp
@@ -243,6 +243,7 @@ void register_server_types() {
 	GDREGISTER_CLASS(RDShaderSPIRV);
 	GDREGISTER_CLASS(RDShaderFile);
 	GDREGISTER_CLASS(RDPipelineSpecializationConstant);
+	GDREGISTER_CLASS(RDAccelerationStructureGeometry);
 
 	GDREGISTER_ABSTRACT_CLASS(RenderData);
 	GDREGISTER_CLASS(RenderDataExtension);

cafecito-games/Foundry

(cherry picked from commit 1b7fcc3993805c5a3b1d601c5286675d07af2908)

servers/register_server_types.cpp
@@ -230,6 +230,7 @@ void register_server_types() {
 	FOUNDRY_REGISTER_CLASS(RDShaderSPIRV);
 	FOUNDRY_REGISTER_CLASS(RDShaderFile);
 	FOUNDRY_REGISTER_CLASS(RDPipelineSpecializationConstant);
+	FOUNDRY_REGISTER_CLASS(RDAccelerationStructureGeometry);
 
 	FOUNDRY_REGISTER_ABSTRACT_CLASS(RenderData);
 	FOUNDRY_REGISTER_CLASS(RenderDataExtension);