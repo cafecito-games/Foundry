Shane-Gadsby/godotwebgpu

Implement support for XR_EXT_user_presence extension

modules/openxr/extensions/openxr_user_presence_extension.h
@@ -1,0 +1,71 @@
+/**************************************************************************/
+/*  openxr_user_presence_extension.h                                      */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "openxr_extension_wrapper.h"
+
+// When supported, the user presence extension allows an application to detect and respond to change
+// of user presence, such as when the user has taken off or put on an XR headset.
+//
+// See: https://registry.khronos.org/OpenXR/specs/1.1/html/xrspec.html#XR_EXT_user_presence
+// for more information.
+
+class OpenXRUserPresenceExtension : public OpenXRExtensionWrapper {
+	GDCLASS(OpenXRUserPresenceExtension, OpenXRExtensionWrapper);
+
+protected:
+	static void _bind_methods() {}
+
+public:
+	static OpenXRUserPresenceExtension *get_singleton();
+
+	OpenXRUserPresenceExtension();
+	virtual ~OpenXRUserPresenceExtension() override;
+
+	virtual HashMap<String, bool *> get_requested_extensions(XrVersion p_version) override;
+	virtual void *set_system_properties_and_get_next_pointer(void *p_next_pointer) override;
+
+	virtual void on_state_ready() override;
+	virtual void on_state_stopping() override;
+
+	virtual bool on_event_polled(const XrEventDataBuffer &event) override;
+
+	bool is_user_present() const;
+	bool is_active() const;
+
+private:
+	static OpenXRUserPresenceExtension *singleton;
+
+	bool available = false;
+	XrSystemUserPresencePropertiesEXT properties;
+
+	bool user_present = true;
+};

modules/openxr/register_types.cpp
@@ -80,6 +80,7 @@
 #ifdef MODULE_GLTF_ENABLED
 #include "extensions/openxr_render_model_extension.h"
 #endif
+#include "extensions/openxr_user_presence_extension.h"
 #include "extensions/openxr_valve_analog_threshold_extension.h"
 #include "extensions/openxr_valve_controller_extension.h"
 #include "extensions/openxr_visibility_mask_extension.h"
@@ -142,6 +143,7 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 		GDREGISTER_CLASS(OpenXRRenderModelExtension);
 #endif
 		GDREGISTER_CLASS(OpenXRAndroidThreadSettingsExtension);
+		GDREGISTER_CLASS(OpenXRUserPresenceExtension);
 
 		// Note, we're not registering all wrapper classes here, there is no point in exposing them
 		// if there isn't specific logic to expose.
@@ -213,6 +215,11 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 			OpenXRAndroidThreadSettingsExtension *android_thread_settings = memnew(OpenXRAndroidThreadSettingsExtension);
 			OpenXRAPI::register_extension_wrapper(android_thread_settings);
 			Engine::get_singleton()->add_singleton(Engine::Singleton("OpenXRAndroidThreadSettingsExtension", android_thread_settings));
+
+			// Register user presence extension as a singleton
+			OpenXRUserPresenceExtension *user_presence_extension = memnew(OpenXRUserPresenceExtension);
+			OpenXRAPI::register_extension_wrapper(user_presence_extension);
+			Engine::get_singleton()->add_singleton(Engine::Singleton("OpenXRUserPresenceExtension", user_presence_extension));
 
 			// register gated extensions
 			if (int(GLOBAL_GET("xr/openxr/extensions/debug_utils")) > 0) {

cafecito-games/Foundry

(cherry picked from commit a182e182115f5dc3b4d37dcdc0b76a1a4b88b116)

modules/openxr/extensions/openxr_user_presence_extension.h
@@ -1,0 +1,71 @@
+/**************************************************************************/
+/*  openxr_user_presence_extension.h                                      */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "openxr_extension_wrapper.h"
+
+// When supported, the user presence extension allows an application to detect and respond to change
+// of user presence, such as when the user has taken off or put on an XR headset.
+//
+// See: https://registry.khronos.org/OpenXR/specs/1.1/html/xrspec.html#XR_EXT_user_presence
+// for more information.
+
+class OpenXRUserPresenceExtension : public OpenXRExtensionWrapper {
+	FOUNDRY_CLASS(OpenXRUserPresenceExtension, OpenXRExtensionWrapper);
+
+protected:
+	static void _bind_methods() {}
+
+public:
+	static OpenXRUserPresenceExtension *get_singleton();
+
+	OpenXRUserPresenceExtension();
+	virtual ~OpenXRUserPresenceExtension() override;
+
+	virtual HashMap<String, bool *> get_requested_extensions(XrVersion p_version) override;
+	virtual void *set_system_properties_and_get_next_pointer(void *p_next_pointer) override;
+
+	virtual void on_state_ready() override;
+	virtual void on_state_stopping() override;
+
+	virtual bool on_event_polled(const XrEventDataBuffer &event) override;
+
+	bool is_user_present() const;
+	bool is_active() const;
+
+private:
+	static OpenXRUserPresenceExtension *singleton;
+
+	bool available = false;
+	XrSystemUserPresencePropertiesEXT properties;
+
+	bool user_present = true;
+};

modules/openxr/register_types.cpp
@@ -75,6 +75,7 @@
 #ifdef MODULE_GLTF_ENABLED
 #include "extensions/openxr_render_model_extension.h"
 #endif
+#include "extensions/openxr_user_presence_extension.h"
 #include "extensions/openxr_valve_analog_threshold_extension.h"
 #include "extensions/openxr_valve_controller_extension.h"
 #include "extensions/openxr_visibility_mask_extension.h"
@@ -134,6 +135,7 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 		FOUNDRY_REGISTER_CLASS(OpenXRRenderModelExtension);
 #endif
 		FOUNDRY_REGISTER_CLASS(OpenXRAndroidThreadSettingsExtension);
+		FOUNDRY_REGISTER_CLASS(OpenXRUserPresenceExtension);
 
 		// Note, we're not registering all wrapper classes here, there is no point in exposing them
 		// if there isn't specific logic to expose.
@@ -205,6 +207,11 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 			OpenXRAndroidThreadSettingsExtension *android_thread_settings = memnew(OpenXRAndroidThreadSettingsExtension);
 			OpenXRAPI::register_extension_wrapper(android_thread_settings);
 			Engine::get_singleton()->add_singleton(Engine::Singleton("OpenXRAndroidThreadSettingsExtension", android_thread_settings));
+
+			// Register user presence extension as a singleton
+			OpenXRUserPresenceExtension *user_presence_extension = memnew(OpenXRUserPresenceExtension);
+			OpenXRAPI::register_extension_wrapper(user_presence_extension);
+			Engine::get_singleton()->add_singleton(Engine::Singleton("OpenXRUserPresenceExtension", user_presence_extension));
 
 			// register gated extensions
 			if (int(GLOBAL_GET("xr/openxr/extensions/debug_utils")) > 0) {